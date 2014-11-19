e4fbeebfd4ebcd903891002ed86c37afb697a425
Steve Orvell <steve@orvellia.com>
2014-11-18 19:01:21 -0800

    Fixes #60.

--- src/HTMLImports/parser.js
+++ src/HTMLImports/parser.js
@@ -168,12 +168,7 @@ var importParser = {
 
   addElementToDocument: function(elt) {
     var port = this.rootImportForElement(elt.__importElement || elt);
-    var l = port.__insertedElements = port.__insertedElements || 0;
-    var refNode = port.nextElementSibling;
-    for (var i=0; i < l; i++) {
-      refNode = refNode && refNode.nextElementSibling;
-    }
-    port.parentNode.insertBefore(elt, refNode);
+    port.parentNode.insertBefore(elt, port);
   },
 
   // tracks when a loadable element has loaded
