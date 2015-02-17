cff106ecd6a2000854280addf69a8025ce0005a8
Victor Berchet <victor@suumit.com>
2015-02-17 17:06:53 +0100

    style(shadowCSS) add a missing ";"

--- src/ShadowCSS/ShadowCSS.js
+++ src/ShadowCSS/ShadowCSS.js
@@ -495,7 +495,7 @@ var ShadowCSS = {
         // remove :host since it should be unnecessary
         var t = p.trim().replace(polyfillHostRe, '');
         if (t && (splits.indexOf(t) < 0) && (t.indexOf(attrName) < 0)) {
-          p = t.replace(/([^:]*)(:*)(.*)/, '$1' + attrName + '$2$3')
+          p = t.replace(/([^:]*)(:*)(.*)/, '$1' + attrName + '$2$3');
         }
         return p;
       }).join(sep);
