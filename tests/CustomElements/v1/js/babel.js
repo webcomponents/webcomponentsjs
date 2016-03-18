d10b60f31876aba467b6e854c47bf594c0c747f1
Justin Fagnani <justinfagnani@google.com>
2016-03-18 00:16:19 -0700

    ES5, Babel and Typescript tests

---
+++ tests/CustomElements/v1/js/babel.js
@@ -0,0 +1,81 @@
+/**
+ * @license
+ * Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+suite('babel', function() {
+
+  // Fails because the XTypescript constructor does not return the result of
+  // the super call. See: https://github.com/Microsoft/TypeScript/issues/7574
+  test('document.defineElement create typescript generated ES5 via new', function() {
+    'use strict';
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+    function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+    function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+    var XBabel = function (_HTMLElement) {
+      _inherits(XBabel, _HTMLElement);
+
+      function XBabel() {
+        _classCallCheck(this, XBabel);
+
+        CustomElements.setCurrentTag('x-babel');
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(XBabel).call(this));
+      }
+
+      return XBabel;
+    }(HTMLElement);
+
+    // register x-foo
+    document.defineElement('x-babel', XBabel);
+    // create an instance via new
+    var e = new XBabel();
+    console.log(e);
+    // test localName
+    assert.equal(e.localName, 'x-babel');
+    // test instanceof
+    assert.instanceOf(e, XBabel);
+  });
+
+  test('document.defineElement create typescript generated ES5 via createElement', function() {
+    'use strict';
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+    function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+    function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+    var XBabel2 = function (_HTMLElement) {
+      _inherits(XBabel2, _HTMLElement);
+
+      function XBabel2() {
+        _classCallCheck(this, XBabel2);
+
+        CustomElements.setCurrentTag('x-babel2');
+        return _possibleConstructorReturn(this, Object.getPrototypeOf(XBabel2).call(this));
+      }
+
+      return XBabel2;
+    }(HTMLElement);
+
+    // register x-foo
+    document.defineElement('x-babel2', XBabel2);
+    // create an instance via new
+    var e = document.createElement('x-babel2');
+    console.log(e);
+    // test localName
+    assert.equal(e.localName, 'x-babel2');
+    // test instanceof
+    assert.instanceOf(e, XBabel2);
+  });
+
+});
