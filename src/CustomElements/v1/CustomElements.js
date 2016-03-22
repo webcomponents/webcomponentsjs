a2b4b1fe62951d8738603c3b494c427fc211c6f5
Justin Fagnani <justinfagnani@google.com>
2016-03-22 12:54:32 -0700

    Reorganize to use CustomElementsRegistry proto

--- src/CustomElements/v1/CustomElements.js
+++ src/CustomElements/v1/CustomElements.js
@@ -27,176 +27,6 @@
 (function() {
   'use strict';
 
-  window.CustomElements = {};
-
-  function CustomElementsRegistry() {}
-  CustomElementsRegistry.prototype.define = function(name, constructor, options) {
-    // 5.1.1
-    if (typeof constructor !== 'function') {
-      throw new TypeError('constructor must be a Constructor');
-    }
-
-    // 5.1.2
-    name = name.toString().toLowerCase();
-
-    // 5.1.3
-    if (!customNameValidation.test(name)) {
-      throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The element name is not valid.`);
-    }
-    if (isReservedTag(name)) {
-      throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The element name is reserved.`);
-    }
-
-    // 5.1.4? Can't polyfill?
-
-    // 5.1.5
-    if (registry.has(name)) {
-      throw new Error(`NotSupportedError: Document.defineElement an element with name '${name}' is already registered`);
-    }
-
-    // 5.1.6
-    // IE11 doesn't support Map.values, only Map.forEach
-    registry.forEach(function(value, key) {
-      if (value.constructor === constructor) {
-        throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The constructor is already used.`);
-      }
-    });
-
-    // 5.1.7
-    var localName = name;
-
-    // 5.1.8
-    var _extends = options && options.extends || '';
-
-    // 5.1.9
-    if (_extends !== null) {
-      // skip for now
-    }
-
-    // 5.1.10, 5.1.11
-    var observedAttributes = constructor.observedAttributes || [];
-
-    // 5.1.12
-    var prototype = constructor.prototype;
-
-    // 5.1.13?
-
-    // 5.1.14
-    var connectedCallback = prototype.connectedCallback;
-    // 5.1.15
-    checkCallback(connectedCallback, localName, 'connectedCallback');
-    // 5.1.16
-    var disconnectedCallback = prototype.disconnectedCallback;
-    // 5.1.17
-    checkCallback(disconnectedCallback, localName, 'disconnectedCallback');
-    // 5.1.18
-    var attributeChangedCallback = prototype.attributeChangedCallback;
-    // 5.1.19
-    checkCallback(attributeChangedCallback, localName, 'attributeChangedCallback');
-
-    // 5.1.20
-    // @type {Definition}
-    var definition = {
-      name: name,
-      localName: localName,
-      constructor: constructor,
-      connectedCallback: connectedCallback,
-      disconnectedCallback: disconnectedCallback,
-      attributeChangedCallback: attributeChangedCallback,
-      observedAttributes: observedAttributes,
-    };
-
-    // 5.1.21
-    registry.set(localName, definition);
-
-    // 5.1.22
-    // The spec says we should upgrade all existing elements now, but if we
-    // defer we can do less tree walks
-    scheduleUpgrade();
-  };
-
-  window.customElements = new CustomElementsRegistry();
-
-  function observeRoot(root) {
-    if (!root.__observer) {
-      var observer = new MutationObserver(handleMutations);
-      observer.observe(root, {childList: true, subtree: true});
-      root.__observer = observer;
-    }
-    return root.__observer;
-  }
-  var _observer = observeRoot(document);
-
-  CustomElements.flush = function() {
-    handleMutations(_observer.takeRecords());
-  };
-
-  var _newInstance;
-  var _newTagName;
-
-  CustomElements.setCurrentTag = function(tagName) {
-    _newTagName = _newTagName || tagName;
-  }
-
-  function setNewInstance(instance) {
-    console.assert(_newInstance == null);
-    _newInstance = instance;
-  }
-
-  var origHTMLElement = HTMLElement;
-
-  // TODO: patch up all built-in subclasses of HTMLElement to use the fake
-  // HTMLElement.prototype
-  window.HTMLElement = function() {
-    if (_newInstance) {
-      var i = _newInstance;
-      _newInstance = null;
-      _newTagName = null;
-      return i;
-    }
-    if (_newTagName) {
-      var tagName = _newTagName.toLowerCase();
-      _newTagName = null;
-      var element = rawCreateElement(tagName);
-      var definition = registry.get(tagName);
-      if (definition) {
-        _upgradeElement(element, definition, false);
-        return element;
-      }
-    }
-    throw new Error('unknown constructor');
-  }
-  HTMLElement.prototype = Object.create(origHTMLElement.prototype);
-  Object.defineProperty(HTMLElement.prototype, 'constructor', {
-    writable: false,
-    configurable: true,
-    enumerable: false,
-    value: HTMLElement,
-  });
-
-  var rawCreateElement = document.createElement.bind(document);
-  document.createElement = function(tagName) {
-    var element = rawCreateElement(tagName);
-    var definition = registry.get(tagName.toLowerCase());
-    if (definition) {
-      _upgradeElement(element, definition, true);
-    }
-    return element;
-  };
-
-  var HTMLNS = 'http://www.w3.org/1999/xhtml';
-  var _origCreateElementNS = document.createElementNS;
-  document.createElementNS = function(namespaceURI, qualifiedName) {
-    if (namespaceURI === 'http://www.w3.org/1999/xhtml') {
-      return document.createElement(qualifiedName);
-    } else {
-      return _origCreateElementNS(namespaceURI, qualifiedName);
-    }
-  };
-
-  // @type {Map<String, Definition>}
-  var registry = new Map();
-
   var reservedTagList = [
     'annotation-xml',
     'color-profile',
@@ -207,74 +37,8 @@
     'font-face-name',
     'missing-glyph',
   ];
+
   var customNameValidation = /^[a-z][.0-9_a-z]*-[\-.0-9_a-z]*$/;
-
-  var microtaskScheduler = Promise.resolve();
-
-  var upgradeTask = null;
-
-  function scheduleMicrotask(task) {
-    return microtaskScheduler.then(task);
-  }
-
-  function scheduleUpgrade() {
-    if (upgradeTask !== null) {
-      return
-    }
-    upgradeTask = scheduleMicrotask(upgrade);
-  }
-
-  function upgrade() {
-    _upgrade(document.documentElement);
-  }
-
-  function _upgrade(element) {
-    if (element.tagName === 'LINK' && element.import) {
-      _upgrade(element.import.documentElement);
-    } else {
-      var children = element.children;
-      if (element.shadowRoot) {
-        children = children.concat(element.shadowRoot.children);
-      }
-      for (var i = 0; i < children.length; i++) {
-        _upgrade(children[i]);
-      }
-    }
-  }
-
-  var attributeObserver = new MutationObserver(handleAttributeChange);
-  function handleAttributeChange(mutations) {
-    console.log('handleAttributeChange', arguments);
-    for (var i = 0; i < mutations.length; i++) {
-      var mutation = mutations[i];
-      if (mutation.type === 'attributes') {
-        var name = mutation.attributeName;
-        var oldValue = mutation.oldValue;
-        var target = mutation.target;
-        var newValue = target.getAttribute(name);
-        var namespace = mutation.attributeNamespace;
-        target.attributeChangedCallback(name, oldValue, newValue, namespace);
-      }
-    }
-  }
-
-  function _upgradeElement(element, definition, callConstructor) {
-    var prototype = definition.constructor.prototype;
-    Object.setPrototypeOf(element, prototype);
-    if (callConstructor) {
-      setNewInstance(element);
-      element.__upgraded = true;
-      new (definition.constructor)();
-      console.assert(_newInstance == null);
-    }
-    if (definition.attributeChangedCallback && definition.observedAttributes.length > 0) {
-      attributeObserver.observe(element, {
-        attributes: true,
-        attributeOldValue: true,
-        attributeFilter: definition.observedAttributes,
-      });
-    }
-  }
 
   function checkCallback(callback, elementName, calllbackName) {
     if (callback !== undefined && typeof callback !== 'function') {
@@ -287,70 +51,267 @@
     return reservedTagList.indexOf(name) !== -1;
   }
 
-  var forEach = Array.prototype.forEach.call.bind(Array.prototype.forEach);
+  function CustomElementsRegistry() {
+    // @type {Map<String, Definition>}
+    this._definitions = new Map();
+    this._observer = this._observeRoot(document);
+    this._attributeObserver =
+        new MutationObserver(this._handleAttributeChange.bind(this));
+    this._newInstance;
+    this._newTagName;
+    this.polyfilled = true;
+  }
 
-  function handleMutations(mutations) {
-    // console.log('handleMutations', mutations);
-    for (var i = 0; i < mutations.length; i++) {
-      var mutation = mutations[i];
-      if (mutation.type === 'childList') {
-        addNodes(mutation.addedNodes);
-        removeNodes(mutation.removedNodes);
+  CustomElementsRegistry.prototype = {
+    define(name, constructor, options) {
+      // 5.1.1
+      if (typeof constructor !== 'function') {
+        throw new TypeError('constructor must be a Constructor');
       }
-    }
-  }
 
-  function addNodes(nodeList) {
-    for (var i = 0; i < nodeList.length; i++) {
-      var root = nodeList[i];
-      var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
-      do {
-        var node = walker.currentNode;
-        var definition = registry.get(node.localName);
-        if (!definition) {
-          continue;
-        }
-        if (!node.__upgraded) {
-          _upgradeElement(node, definition, true);
-        }
-        if (node.__upgraded && !node.__attached) {
-          node.__attached = true;
-          var definition = registry.get(node.localName);
-          if (definition && definition.connectedCallback) {
-            definition.connectedCallback.call(node);
-          }
-        }
-      } while (walker.nextNode())
-    }
-  }
+      // 5.1.2
+      name = name.toString().toLowerCase();
 
-  function removeNodes(nodeList) {
-    for (var i = 0; i < nodeList.length; i++) {
-      var root = nodeList[i];
-      var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
-      do {
-        var node = walker.currentNode;
-        if (node.__upgraded && node.__attached) {
-          node.__attached = false;
-          var definition = registry.get(node.localName);
-          if (definition && definition.disconnectedCallback) {
-            definition.disconnectedCallback.call(node);
-          }
-        }
-      } while (walker.nextNode())
-    }
-  }
-
-  // recurse up the tree to check if an element is actually in the main document.
-  function inDocument(element) {
-    var p = element;
-    // var doc = window.wrap(document);
-    var doc = document;
-    while (p = p.parentNode || ((p.nodeType === Node.DOCUMENT_FRAGMENT_NODE) && p.host)) {
-      if (p === doc) {
-        return true;
+      // 5.1.3
+      if (!customNameValidation.test(name)) {
+        throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The element name is not valid.`);
       }
+      if (isReservedTag(name)) {
+        throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The element name is reserved.`);
+      }
+
+      // 5.1.4? Can't polyfill?
+
+      // 5.1.5
+      if (this._definitions.has(name)) {
+        throw new Error(`NotSupportedError: Document.defineElement an element with name '${name}' is already registered`);
+      }
+
+      // 5.1.6
+      // IE11 doesn't support Map.values, only Map.forEach
+      this._definitions.forEach(function(value, key) {
+        if (value.constructor === constructor) {
+          throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The constructor is already used.`);
+        }
+      });
+
+      // 5.1.7
+      var localName = name;
+
+      // 5.1.8
+      var _extends = options && options.extends || '';
+
+      // 5.1.9
+      if (_extends !== null) {
+        // skip for now
+      }
+
+      // 5.1.10, 5.1.11
+      var observedAttributes = constructor.observedAttributes || [];
+
+      // 5.1.12
+      var prototype = constructor.prototype;
+
+      // 5.1.13?
+
+      // 5.1.14
+      var connectedCallback = prototype.connectedCallback;
+      // 5.1.15
+      checkCallback(connectedCallback, localName, 'connectedCallback');
+      // 5.1.16
+      var disconnectedCallback = prototype.disconnectedCallback;
+      // 5.1.17
+      checkCallback(disconnectedCallback, localName, 'disconnectedCallback');
+      // 5.1.18
+      var attributeChangedCallback = prototype.attributeChangedCallback;
+      // 5.1.19
+      checkCallback(attributeChangedCallback, localName, 'attributeChangedCallback');
+
+      // 5.1.20
+      // @type {Definition}
+      var definition = {
+        name: name,
+        localName: localName,
+        constructor: constructor,
+        connectedCallback: connectedCallback,
+        disconnectedCallback: disconnectedCallback,
+        attributeChangedCallback: attributeChangedCallback,
+        observedAttributes: observedAttributes,
+      };
+
+      // 5.1.21
+      this._definitions.set(localName, definition);
+
+      // 5.1.22
+      // The spec says we should upgrade all existing elements now, but if we
+      // defer we can do less tree walks
+      // scheduleUpgrade();
+    },
+
+    flush() {
+      this._handleMutations(this._observer.takeRecords());
+    },
+
+    setCurrentTag(tagName) {
+      this._newTagName = this._newTagName || tagName;
+    },
+
+    _setNewInstance(instance) {
+      console.assert(this._newInstance == null);
+      this._newInstance = instance;
+    },
+
+    _observeRoot(root) {
+      if (!root.__observer) {
+        var observer = new MutationObserver(this._handleMutations.bind(this));
+        observer.observe(root, {childList: true, subtree: true});
+        root.__observer = observer;
+      }
+      return root.__observer;
+    },
+
+    _handleMutations(mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        var mutation = mutations[i];
+        if (mutation.type === 'childList') {
+          this._addNodes(mutation.addedNodes);
+          this._removeNodes(mutation.removedNodes);
+        }
+      }
+    },
+
+    _addNodes(nodeList) {
+      for (var i = 0; i < nodeList.length; i++) {
+        var root = nodeList[i];
+        var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
+        do {
+          var node = walker.currentNode;
+          var definition = this._definitions.get(node.localName);
+          if (!definition) {
+            continue;
+          }
+          if (!node.__upgraded) {
+            this._upgradeElement(node, definition, true);
+          }
+          if (node.__upgraded && !node.__attached) {
+            node.__attached = true;
+            if (definition && definition.connectedCallback) {
+              definition.connectedCallback.call(node);
+            }
+          }
+        } while (walker.nextNode())
+      }
+    },
+
+    _removeNodes(nodeList) {
+      for (var i = 0; i < nodeList.length; i++) {
+        var root = nodeList[i];
+        var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
+        do {
+          var node = walker.currentNode;
+          if (node.__upgraded && node.__attached) {
+            node.__attached = false;
+            var definition = this._definitions.get(node.localName);
+            if (definition && definition.disconnectedCallback) {
+              definition.disconnectedCallback.call(node);
+            }
+          }
+        } while (walker.nextNode())
+      }
+    },
+
+    _upgradeElement(element, definition, callConstructor) {
+      var prototype = definition.constructor.prototype;
+      Object.setPrototypeOf(element, prototype);
+      if (callConstructor) {
+        this._setNewInstance(element);
+        element.__upgraded = true;
+        new (definition.constructor)();
+        console.assert(this._newInstance == null);
+      }
+      if (definition.attributeChangedCallback && definition.observedAttributes.length > 0) {
+        this._attributeObserver.observe(element, {
+          attributes: true,
+          attributeOldValue: true,
+          attributeFilter: definition.observedAttributes,
+        });
+      }
+    },
+
+    _handleAttributeChange(mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        var mutation = mutations[i];
+        if (mutation.type === 'attributes') {
+          var name = mutation.attributeName;
+          var oldValue = mutation.oldValue;
+          var target = mutation.target;
+          var newValue = target.getAttribute(name);
+          var namespace = mutation.attributeNamespace;
+          target.attributeChangedCallback(name, oldValue, newValue, namespace);
+        }
+      }
+    },
+
+  };
+
+  function patchHTMLElement(win) {
+    // TODO: patch up all built-in subclasses of HTMLElement to use the fake
+    // HTMLElement.prototype
+    var origHTMLElement = HTMLElement;
+    window.HTMLElement = function() {
+      if (win.customElements._newInstance) {
+        var i = win.customElements._newInstance;
+        win.customElements._newInstance = null;
+        win.customElements._newTagName = null;
+        return i;
+      }
+      if (win.customElements._newTagName) {
+        var tagName = win.customElements._newTagName.toLowerCase();
+        win.customElements._newTagName = null;
+        return win.document._createElement(tagName, false);
+      }
+      throw new Error('unknown constructor');
     }
-    return false;
+    HTMLElement.prototype = Object.create(origHTMLElement.prototype);
+    Object.defineProperty(HTMLElement.prototype, 'constructor', {
+      writable: false,
+      configurable: true,
+      enumerable: false,
+      value: HTMLElement,
+    });
   }
+
+  function patchCreateElement(win) {
+    var doc = win.document;
+    var rawCreateElement = doc.createElement.bind(document);
+    doc._createElement = function(tagName, callConstructor) {
+      var element = rawCreateElement(tagName);
+      var definition = win.customElements._definitions.get(tagName.toLowerCase());
+      if (definition) {
+        win.customElements._upgradeElement(element, definition, callConstructor);
+      }
+      return element;
+    };
+    doc.createElement = function(tagName) {
+      return doc._createElement(tagName, true);
+    }
+  }
+
+  function patchCreateElementNS(win) {
+    var doc = win.document;
+    var HTMLNS = 'http://www.w3.org/1999/xhtml';
+    var _origCreateElementNS = document.createElementNS;
+    doc.createElementNS = function(namespaceURI, qualifiedName) {
+      if (namespaceURI === 'http://www.w3.org/1999/xhtml') {
+        return doc.createElement(qualifiedName);
+      } else {
+        return _origCreateElementNS(namespaceURI, qualifiedName);
+      }
+    };
+  }
+
+  window.customElements = new CustomElementsRegistry();
+  patchHTMLElement(window);
+  patchCreateElement(window);
+  patchCreateElementNS(window);
 })();
