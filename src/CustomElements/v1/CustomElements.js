dd793f3989a0cff4ebd91a27ae2d4aa3ede28102
Justin Fagnani <justinfagnani@google.com>
2016-03-23 13:23:54 -0700

    Some Closure optimizations

--- src/CustomElements/v1/CustomElements.js
+++ src/CustomElements/v1/CustomElements.js
@@ -26,6 +26,9 @@ var CustomElementDefinition;
 (function() {
   'use strict';
 
+  var doc = document;
+  var win = window;
+
   /**
    * @const
    * @type {Array<string>}
@@ -46,10 +49,12 @@ var CustomElementDefinition;
    */
   var customNameValidation = /^[a-z][.0-9_a-z]*-[\-.0-9_a-z]*$/;
 
-  function checkCallback(callback, elementName, calllbackName) {
+  function getCallback(proto, calllbackName, elementName) {
+    var callback = proto[calllbackName];
     if (callback !== undefined && typeof callback !== 'function') {
       throw new Error(`TypeError: ${elementName} '${calllbackName}' is not a Function`);
     }
+    return callback;
   }
 
   function isReservedTag(name) {
@@ -88,24 +93,24 @@ var CustomElementDefinition;
 
       // 5.1.3
       if (!customNameValidation.test(name)) {
-        throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The element name is not valid.`);
+        throw new Error(`NotSupportedError: document.defineElement failed for '${name}'. The element name is not valid.`);
       }
       if (isReservedTag(name)) {
-        throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The element name is reserved.`);
+        throw new Error(`NotSupportedError: document.defineElement failed for '${name}'. The element name is reserved.`);
       }
 
       // 5.1.4? Can't polyfill?
 
       // 5.1.5
       if (this._definitions.has(name)) {
-        throw new Error(`NotSupportedError: Document.defineElement an element with name '${name}' is already registered`);
+        throw new Error(`NotSupportedError: document.defineElement an element with name '${name}' is already registered`);
       }
 
       // 5.1.6
       // IE11 doesn't support Map.values, only Map.forEach
       this._definitions.forEach(function(value, key) {
         if (value.constructor === constructor) {
-          throw new Error(`NotSupportedError: Document.defineElement failed for '${name}'. The constructor is already used.`);
+          throw new Error(`NotSupportedError: document.defineElement failed for '${name}'. The constructor is already used.`);
         }
       });
 
@@ -128,18 +133,12 @@ var CustomElementDefinition;
 
       // 5.1.13?
 
-      // 5.1.14
-      var connectedCallback = prototype['connectedCallback'];
-      // 5.1.15
-      checkCallback(connectedCallback, localName, 'connectedCallback');
-      // 5.1.16
-      var disconnectedCallback = prototype['disconnectedCallback'];
-      // 5.1.17
-      checkCallback(disconnectedCallback, localName, 'disconnectedCallback');
-      // 5.1.18
-      var attributeChangedCallback = prototype['attributeChangedCallback'];
-      // 5.1.19
-      checkCallback(attributeChangedCallback, localName, 'attributeChangedCallback');
+      // 5.1.14 & 5.1.15
+      var connectedCallback = getCallback(prototype, 'connectedCallback', localName);
+      // 5.1.16 & 5.1.17
+      var disconnectedCallback = getCallback(prototype, 'disconnectedCallback', localName);
+      // 5.1.18 & 5.1.19
+      var attributeChangedCallback = getCallback(prototype, 'attributeChangedCallback', localName);
 
       // 5.1.20
       // @type {CustomElementDefinition}
@@ -158,7 +157,7 @@ var CustomElementDefinition;
 
       // 5.1.22
       // this causes an upgrade of the document
-      this._addNodes(document.childNodes);
+      this._addNodes(doc.childNodes);
     },
 
     flush() {
@@ -198,7 +197,7 @@ var CustomElementDefinition;
     _addNodes(nodeList) {
       for (var i = 0; i < nodeList.length; i++) {
         var root = nodeList[i];
-        var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
+        var walker = doc.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
         do {
           var node = /** @type {HTMLElement} */ (walker.currentNode);
           var definition = this._definitions.get(node.localName);
@@ -224,7 +223,7 @@ var CustomElementDefinition;
     _removeNodes(nodeList) {
       for (var i = 0; i < nodeList.length; i++) {
         var root = nodeList[i];
-        var walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
+        var walker = doc.createTreeWalker(root, NodeFilter.SHOW_ELEMENT);
         do {
           var node = walker.currentNode;
           if (node.__upgraded && node.__attached) {
@@ -289,18 +288,19 @@ var CustomElementDefinition;
 
   // TODO: patch up all built-in subclasses of HTMLElement to use the fake
   // HTMLElement.prototype
-  var origHTMLElement = window.HTMLElement;
-  window.HTMLElement = function() {
-    if (window['customElements']._newInstance) {
-      var i = window['customElements']._newInstance;
-      window['customElements']._newInstance = null;
-      window['customElements']._newTagName = null;
+  var origHTMLElement = win.HTMLElement;
+  win.HTMLElement = function() {
+    var customElements = win['customElements'];
+    if (customElements._newInstance) {
+      var i = customElements._newInstance;
+      customElements._newInstance = null;
+      customElements._newTagName = null;
       return i;
     }
-    if (window['customElements']._newTagName) {
-      var tagName = window['customElements']._newTagName.toLowerCase();
-      window['customElements']._newTagName = null;
-      return window.document._createElement(tagName, false);
+    if (customElements._newTagName) {
+      var tagName = customElements._newTagName.toLowerCase();
+      customElements._newTagName = null;
+      return doc._createElement(tagName, false);
     }
     throw new Error('unknown constructor');
   }
@@ -312,33 +312,34 @@ var CustomElementDefinition;
     value: HTMLElement,
   });
 
-  // patch document.createElement
+  // patch doc.createElement
 
-  var rawCreateElement = document.createElement.bind(document);
-  document._createElement = function(tagName, callConstructor) {
+  var rawCreateElement = doc.createElement.bind(document);
+  doc._createElement = function(tagName, callConstructor) {
+    var customElements = win['customElements'];
     var element = rawCreateElement.call(document, tagName);
-    var definition = window['customElements']._definitions.get(tagName.toLowerCase());
+    var definition = customElements._definitions.get(tagName.toLowerCase());
     if (definition) {
-      window['customElements']._upgradeElement(element, definition, callConstructor);
+      customElements._upgradeElement(element, definition, callConstructor);
     }
     return element;
   };
-  document.createElement = function(tagName) {
-    return document._createElement(tagName, true);
+  doc.createElement = function(tagName) {
+    return doc._createElement(tagName, true);
   }
 
-  // patch document.createElementNS
+  // patch doc.createElementNS
 
   var HTMLNS = 'http://www.w3.org/1999/xhtml';
-  var _origCreateElementNS = document.createElementNS;
-  document.createElementNS = function(namespaceURI, qualifiedName) {
+  var _origCreateElementNS = doc.createElementNS;
+  doc.createElementNS = function(namespaceURI, qualifiedName) {
     if (namespaceURI === 'http://www.w3.org/1999/xhtml') {
-      return document.createElement(qualifiedName);
+      return doc.createElement(qualifiedName);
     } else {
       return _origCreateElementNS.call(document, namespaceURI, qualifiedName);
     }
   };
 
   /** @type {CustomElementsRegistry} */
-  window['customElements'] = new (window['CustomElementsRegistry'])();
+  window['customElements'] = new CustomElementsRegistry();
 })();
