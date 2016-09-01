39e9e1f3ad3a5d4d7a94a362b165e425de6755c1
Daniel Freedman <dfreedm@google.com>
2016-08-31 17:17:32 -0700

    [ci skip] remove module ouroboros

--- gulpfile.js
+++ gulpfile.js
@@ -146,7 +146,7 @@ gulp.task('default', function(cb) {
 });
 
 gulp.task('ShadyCSS', function() {
-  return gulp.src('./src/ShadyCSS/*.js', {base: './'})
+  return gulp.src(['./src/ShadyCSS/*.js', '!./src/ShadyCSS/ShadyCSS.min.js'], {base: './'})
     .pipe(sourcemaps.init())
     .pipe(closureCompiler({
       compilation_level: 'SIMPLE',
@@ -161,7 +161,7 @@ gulp.task('ShadyCSS', function() {
 });
 
 gulp.task('ShadyDOM', function() {
-  return gulp.src('./src/ShadyDOM/*.js', {base: './'})
+  return gulp.src(['./src/ShadyDOM/*.js', '!./src/ShadyDOM/ShadyDOM.min.js'], {base: './'})
     .pipe(sourcemaps.init())
     .pipe(closureCompiler({
       compilation_level: 'SIMPLE',
