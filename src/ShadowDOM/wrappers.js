146f21d9abaa93c7495fe78d670a99e9363ef72b
Josh Wymer <wymetyme@gmail.com>
2016-10-16 12:12:13 -0500

    fix error on iOS 10

--- src/ShadowDOM/wrappers.js
+++ src/ShadowDOM/wrappers.js
@@ -182,6 +182,11 @@ window.ShadowDOMPolyfill = {};
 
   function getDescriptor(source, name) {
     try {
+      // getOwnPropertyDescriptor in Safari on iOS 10.0.1 returns
+      // undefined for these values, which would cause an exception later.
+      if (source === window && name === 'showModalDialog') {
+        return dummyDescriptor;
+      }
       return Object.getOwnPropertyDescriptor(source, name);
     } catch (ex) {
       // JSC and V8 both use data properties instead of accessors which can
