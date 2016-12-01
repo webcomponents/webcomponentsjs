130ecb3bf345433fb6b75a6893ee76f3bf504806
Steven Orvell <sorvell@google.com>
2016-12-01 11:13:12 -0800

    move ES6 polyfills into `lang`

--- src/WebComponents/dom.js
+++ src/WebComponents/dom.js
@@ -128,31 +128,4 @@
     window.MouseEvent.prototype = origMouseEvent.prototype;
   }
 
-  if (!Array.from) {
-    Array.from = function (object) {
-      return [].slice.call(object);
-    };
-  }
-
-  if (!Object.assign) {
-    function assign(target, source) {
-      var n$ = Object.getOwnPropertyNames(source);
-      for (var i=0, p; i < n$.length; i++) {
-        p = n$[i];
-        target[p] = source[p];
-      }
-    }
-
-    Object.assign = function(target, sources) {
-      var args = [].slice.call(arguments, 1);
-      for (var i=0, s; i < args.length; i++) {
-        s = args[i];
-        if (s) {
-          assign(target, s);
-        }
-      }
-      return target;
-    }
-  }
-
 })(window.WebComponents);
