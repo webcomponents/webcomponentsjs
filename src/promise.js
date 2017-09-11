6a1c213ee051a526d7ee43c2f578f65ecdd10911
Daniel Freedman <dfreedm@google.com>
2017-09-11 15:42:13 -0700

    reduce promise polyfill size slightly, comment on closure fix

--- src/promise.js
+++ src/promise.js
@@ -8,7 +8,7 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 'use strict';
-import ES6Promise from '../node_modules/es6-promise/lib/es6-promise.js';
+import ES6Promise from '../node_modules/es6-promise/lib/es6-promise/promise.js';
 
 /*
 Assign the ES6 promise polyfill to window ourselves instead of using the "auto" polyfill
@@ -16,5 +16,6 @@ to work around https://github.com/webcomponents/webcomponentsjs/issues/837
 */
 if (!window.Promise) {
   window.Promise = ES6Promise;
+  // save catch function with a string name to prevent renaming and dead code eliminiation with closure
   ES6Promise.prototype['catch'] = ES6Promise.prototype.catch;
 }
