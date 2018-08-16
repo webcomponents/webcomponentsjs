2cd42dce2136060abb82c6c8400e8e2b75bd7329
Daniel Freedman <dfreedm@google.com>
2018-08-15 17:38:34 -0700

    Always use MutationObserver for promise polyfill
    Also, only save the necessary API from Promise

--- src/promise.js
+++ src/promise.js
@@ -10,43 +10,31 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 'use strict';
 import PromisePolyfill from '../node_modules/promise-polyfill/src/index.js';
 
-/*
-Assign the ES6 promise polyfill to window ourselves instead of using the "auto" polyfill
-to work around https://github.com/webcomponents/webcomponentsjs/issues/837
-*/
 if (!window.Promise) {
   window.Promise = PromisePolyfill;
-  // save Promise API
-  /* eslint-disable no-self-assign */
-  // PromisePolyfill.prototype['catch'] = PromisePolyfill.prototype.catch;
-  PromisePolyfill.prototype['then'] = PromisePolyfill.prototype.then;
-  // PromisePolyfill.prototype['finally'] = PromisePolyfill.prototype.finally;
+  // save Promise API that closure renames
   PromisePolyfill['all'] = PromisePolyfill.all;
   PromisePolyfill['race'] = PromisePolyfill.race;
-  PromisePolyfill['resolve'] = PromisePolyfill.resolve;
-  PromisePolyfill['reject'] = PromisePolyfill.reject;
   /* eslint-enable */
 
-  if (!window.setImmediate) {
-    // approach copied from https://github.com/Polymer/polymer/blob/v3.0.2/lib/utils/async.js
-    const node = document.createTextNode('');
-    const twiddleNode = function twiddleNode() {
-      node.textContent = node.textContent.length > 0 ? '' : 'a';
-    };
-    /** @type {!Array<function():void>} */
-    const callbacks = [];
-    (new MutationObserver(() => {
-      const len = callbacks.length;
-      for (let i = 0; i < len; i++) {
-        callbacks[i]();
-      }
-      callbacks.splice(0, len);
-    }).observe(node, {characterData: true}));
-
-    // set _immediateFn to a MutationObserver for close-to-native timing
-    PromisePolyfill._immediateFn = (fn) => {
-      callbacks.push(fn);
-      twiddleNode();
+  // approach copied from https://github.com/Polymer/polymer/blob/v3.0.2/lib/utils/async.js
+  const node = document.createTextNode('');
+  const twiddleNode = function twiddleNode() {
+    node.textContent = node.textContent.length > 0 ? '' : 'a';
+  };
+  /** @type {!Array<function():void>} */
+  const callbacks = [];
+  (new MutationObserver(() => {
+    const len = callbacks.length;
+    for (let i = 0; i < len; i++) {
+      callbacks[i]();
     }
-  }
+    callbacks.splice(0, len);
+  }).observe(node, {characterData: true}));
+
+  // set _immediateFn to a MutationObserver for close-to-native timing
+  PromisePolyfill._immediateFn = (fn) => {
+    callbacks.push(fn);
+    twiddleNode();
+  };
 }
